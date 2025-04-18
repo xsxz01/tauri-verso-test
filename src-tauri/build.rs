5fafdcb5a58f86950a076be09cbecabd69f4bb46
Pang <pangyuyu@illusiontech.cn>
2025-04-18 12:38:40 +0800

    集成verso

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,4 @@
 fn main() {
+    tauri_runtime_verso_build::get_verso_as_external_bin().unwrap();
     tauri_build::build()
 }
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,4 @@
+use tauri_runtime_verso::{VersoRuntime, INVOKE_SYSTEM_SCRIPTS};
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -6,7 +7,13 @@ fn greet(name: &str) -> String {
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    tauri::Builder::default()
+    // tauri::Builder::default()
+    //     .plugin(tauri_plugin_opener::init())
+    //     .invoke_handler(tauri::generate_handler![greet])
+    //     .run(tauri::generate_context!())
+    //     .expect("error while running tauri application");
+    tauri::Builder::<VersoRuntime>::new()
+        .invoke_system(INVOKE_SYSTEM_SCRIPTS)
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
