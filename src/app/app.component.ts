264b4dedc9ef38574e419294d4bc2f36b5fd77c6
Pang <pangyuyu@illusiontech.cn>
2025-08-28 11:50:26 +0800

    已创建基础界面，现在存在的问题：不兼容动态路由

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 import { invoke } from "@tauri-apps/api/core";
 
 @Component({
     selector: 'app-root',
-    imports: [],
+    imports: [RouterOutlet],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from "@angular/router";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'page',
+        pathMatch: 'full'
+    },
+    {
+        path: 'page',
+        loadComponent: () => import('./pages/page.component').then((mod) => mod.PageComponent),
+        children: [
+            {
+                path: '',
+                redirectTo: 'home',
+                pathMatch: 'full'
+            },
+            {
+                path: 'home',
+                loadComponent: () => import('./pages/home/home.component').then((mod) => mod.HomeComponent),
+            }
+        ]
+    }
+
+];
